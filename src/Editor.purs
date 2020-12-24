2907fc3980181efa8961dcbf662f4df291621efb
Advait Shinde <advait.shinde@gmail.com>
2020-12-24 12:12:56 -0800

    UI evals single lines.

--- src/Editor.purs
+++ src/Editor.purs
@@ -2,7 +2,9 @@ module Editor where
 
 import Prelude
 import ContentEditable as ContentEditable
+import Data.Either (Either(..))
 import Data.Maybe (fromMaybe, Maybe(..))
+import Interpreter (evalProgram')
 import React.Basic.Classic as React
 import React.Basic.DOM as R
 import React.Basic.DOM.Events as Events
@@ -21,24 +23,31 @@ editor =
     { initialState: { counter: 0, html: Nothing }
     , render:
         \self ->
-          R.div
-            { className: "editor " <> self.props.className
-            , children:
-                [ R.div
-                    { className: "columns"
-                    , children:
-                        [ ContentEditable.component
-                            { html: self.props.initialValue `fromMaybe` self.state.html
-                            , onChange: \e -> self.setState (\s -> s { counter = s.counter + 1, html = e })
-                            , className: "editor-left column is-two-thirds"
-                            }
-                        , R.div
-                            { onClick: Events.capture_ $ self.setState (\s -> s { counter = s.counter + 1 })
-                            , children: [ R.text (self.props.initialValue <> ": " <> show self.state.counter) ]
-                            , className: "editor-right column"
-                            }
-                        ]
-                    }
-                ]
-            }
+          let
+            program = self.props.initialValue `fromMaybe` self.state.html
+
+            output = case evalProgram' program of
+              Left err -> show err
+              Right val -> show val
+          in
+            R.div
+              { className: "editor " <> self.props.className
+              , children:
+                  [ R.div
+                      { className: "columns"
+                      , children:
+                          [ ContentEditable.component
+                              { html: program
+                              , onChange: \e -> self.setState (\s -> s { counter = s.counter + 1, html = e })
+                              , className: "editor-left column is-two-thirds"
+                              }
+                          , R.div
+                              { onClick: Events.capture_ $ self.setState (\s -> s { counter = s.counter + 1 })
+                              , children: [ R.text (show output) ]
+                              , className: "editor-right column"
+                              }
+                          ]
+                      }
+                  ]
+              }
     }

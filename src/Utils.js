4ceab4e668c8e238d87e37b150dd3e39527d9590
Advait Shinde <advait.shinde@gmail.com>
2020-12-24 11:51:41 -0800

    Remove unused debug code.

--- src/Utils.js
+++ src/Utils.js
@@ -1,28 +1,8 @@
 "use strict";
 
-exports.debugLog = function (a) {
-  console.log(a);
-  return a;
-};
-
 exports.debugLogAlt = function (a) {
   console.log(a);
   return function (b) {
     return b;
   };
 };
-
-exports.eventTargetTextContent = function (e) {
-  console.log(e.target);
-  return e.target.textContent;
-};
-
-exports.refireEvent = function (e) {
-  setTimeout(function () {
-    var newE = new InputEvent("delayedinput", e);
-    e.target.dispatchEvent(newE);
-  }, 0);
-  return function (b) {
-    return b;
-  };
-}
--- src/Utils.purs
+++ src/Utils.purs
@@ -4,17 +4,9 @@ import Prelude
 import Data.BigNumber (BigNumber)
 import Data.Generic.Rep (Argument(..), Constructor(..))
 import Data.Generic.Rep as Generic
-import Web.Event.Internal.Types (Event)
 
 -- | Create a constant `BigNumber` from a `String`
 bigNum :: String -> BigNumber
 bigNum = Generic.to <<< Constructor <<< Argument
 
--- | Logs the item to the console and returns it.
-foreign import debugLog :: forall a. a -> a
-
 foreign import debugLogAlt :: forall a b. a -> b -> b
-
-foreign import eventTargetTextContent :: Event -> String
-
-foreign import refireEvent :: forall a. Event -> a -> a

f2a2314110ef79a47744ab227436d29ad958168d
Advait Shinde <advait.shinde@gmail.com>
2021-10-11 16:30:15 -0700

    Absolute positioning of results with JS:
      - Allow for copy-on-click for results.

--- dev/editor.jsx
+++ dev/editor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Editor from 'react-simple-code-editor';
 
 const Interpreter = require('../output/Interpreter');
@@ -6,7 +6,12 @@ const EditorPS = require('../output/Editor');
 const Tokenizer = require('../output/Tokenizer');
 
 export default function (props) {
+  const editorRef = useRef(null);
   const [value, setValue] = useState(props.initialValue || "");
+
+  const fontSizePx = props.fontSizePx || 16;
+  const lineHeight = props.lineHeight || 4.5;
+  const lineHeightPx = fontSizePx * lineHeight;
 
   const { highlight, results } = EditorPS.run(value);
 
@@ -16,16 +21,22 @@ export default function (props) {
   };
 
   return (
-    <div className="editor">
-      <div className="editor-left">
-        <Editor
-          value={value}
-          onValueChange={setValueLS}
-          highlight={() => highlight}
-        />
-      </div>
-      <div className="editor-right">
-        {results.map(res => (<div>{res || '\u00A0'}</div>))}
+    <div>
+      <div style={{height: lineHeightPx + "px"}}></div>
+      <div className="editor" style={{fontSize: fontSizePx + "px"}}>
+        <div className="editor-left" ref={editorRef}>
+          <Editor
+            value={value}
+            onValueChange={setValueLS}
+            highlight={() => highlight}
+            style={{lineHeight: lineHeight}}
+          />
+        </div>
+        {results.map((res, i) => {
+          const offset = i * lineHeightPx + lineHeightPx * .8;
+          const style = {top: offset + "px", left: "2em"};
+          return (<div className="result" style={style}>{res || '\u00A0'}</div>);
+        })}
       </div>
     </div>
   )

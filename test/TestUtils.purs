1023dfb3c3cd7a38067425952c6aff7a632e74e2
Advait Shinde <advait.shinde@gmail.com>
2021-10-11 20:43:22 -0700

    Refactor tests to use quickCheckParser.

--- test/TestUtils.purs
+++ test/TestUtils.purs
@@ -3,7 +3,10 @@ module TestUtils where
 import Prelude
 import Control.Monad.Error.Class (class MonadThrow, throwError)
 import Data.Either (Either(..))
+import Effect.Aff (Aff)
 import Effect.Exception (Error, error)
+import Test.QuickCheck (class Arbitrary, Result(..), (===))
+import Test.Spec.QuickCheck (quickCheck)
 import Text.Parsing.Parser (Parser, runParser)
 
 -- | Runs the given parser or fails the test.
@@ -15,3 +18,16 @@ runParserOrFail ::
 runParserOrFail input p = case runParser input p of
   Right res -> pure res
   Left err -> throwError $ error ("Failed to parse " <> show input <> ": " <> show err)
+
+-- | Helper to quickcheck the given parser.
+quickCheckParser :: forall p. Arbitrary p => (p -> String) -> Parser String String -> Aff Unit
+quickCheckParser genInput parser =
+  quickCheck
+    ( \(p :: p) ->
+        let
+          input = genInput p
+        in
+          case runParser input parser of
+            Left err -> Failed $ show err
+            Right parsed -> parsed === input
+    )
--- test/Tokenizer.purs
+++ test/Tokenizer.purs
@@ -4,10 +4,9 @@ import Prelude
 import Data.Either (Either(..))
 import Data.Formatter.Number (format, Formatter(..))
 import Data.Int (toNumber)
-import Test.QuickCheck (Result(..), (===))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
-import Test.Spec.QuickCheck (quickCheck)
+import TestUtils (quickCheckParser)
 import Text.Parsing.Parser (Parser, runParser)
 import Tokenizer (TokenType(..), tokenStreamParser)
 
@@ -15,21 +14,15 @@ spec :: Spec Unit
 spec = do
   describe "Parsing" do
     it "parses integers" do
-      quickCheck \(n :: Int) -> case runParser (show n) numberParser of
-        Left err -> Failed $ show err
-        Right parsed -> parsed === (show n)
+      quickCheckParser (\(n :: Int) -> show n) numberParser
     it "parses integers with commas" do
-      quickCheck \(n :: Int) ->
-        let
-          formatted = format (Formatter { abbreviations: false, after: 0, before: 10, comma: true, sign: false }) (toNumber n)
-        in
-          case runParser formatted numberParser of
-            Left err -> Failed $ show err
-            Right parsed -> parsed === formatted
+      quickCheckParser
+        ( \(n :: Int) ->
+            format (Formatter { abbreviations: false, after: 0, before: 10, comma: true, sign: false }) (toNumber n)
+        )
+        numberParser
     it "parses floats" do
-      quickCheck \(n :: Number) -> case runParser (show n) numberParser of
-        Left err -> Failed $ show err
-        Right parsed -> parsed === (show n)
+      quickCheckParser (\(n :: Number) -> show n) numberParser
     it "parses unknown characters" do
       runParser "~" tokenStreamParser `shouldEqual` Right [ UnknownTk "~" ]
 

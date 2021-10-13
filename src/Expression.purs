8c9d121d45ed4aa68e1ad2273fe6c91f6800feb5
Advait Shinde <advait.shinde@gmail.com>
2021-10-13 12:53:02 -0700

    Randomly generate/fuzz exprs for testing.

--- src/Expression.purs
+++ src/Expression.purs
@@ -3,6 +3,11 @@ module Expression where
 import Prelude
 import Data.Array as Array
 import Data.BigNumber (BigNumber)
+import Data.NonEmpty ((:|))
+import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
+import Test.QuickCheck.Gen (Gen)
+import Test.QuickCheck.Gen as Gen
+import Utils (parseBigNumber)
 
 -- | Represents an expression that can be evaluated.
 data ParsedExpr
@@ -26,10 +31,48 @@ instance showParsedExpr :: Show ParsedExpr where
   show (Name n) = n
   show (Fn1 { name, p1 }) = name <> "(" <> show p1 <> ")"
   show (Fn2 { name, p1, p2 })
-    | Array.elem name infixNames = show p1 <> name <> show p2
+    | Array.elem name infixNames = showWrapped p1 <> name <> showWrapped p2
     | otherwise = name <> "(" <> show p1 <> "," <> show p2 <> ")"
   show (BindPrefix { name, expr }) = "prefix " <> name <> " = " <> show expr
   show (BindRootUnit { name }) = "unit " <> name
   show (BindUnit { name, expr }) = "unit " <> name <> " = " <> show expr
   show (BindAlias { name, target }) = "alias " <> name <> " = " <> target
   show (BindVariable { name, expr }) = name <> " = " <> show expr
+
+-- | Wraps the expression in parentheses if it's a compound expression.
+showWrapped :: ParsedExpr -> String
+showWrapped (Scalar n) = show n
+
+showWrapped (Name n) = n
+
+showWrapped e = "(" <> show e <> ")"
+
+-- | TODO(advait): See if we can move Arbitrary to the test package.
+instance arbitraryParsedExpr :: Arbitrary ParsedExpr where
+  arbitrary = genExpr 3
+
+genBigNumber :: Gen BigNumber
+genBigNumber = parseBigNumber <$> show <$> (arbitrary :: Gen Int)
+
+genScalar :: Gen ParsedExpr
+genScalar = Scalar <$> genBigNumber
+
+genName :: Gen ParsedExpr
+genName = Name <$> Gen.elements ("ft" :| [ "pi", "m", "advait" ])
+
+genConcrete :: Gen ParsedExpr
+genConcrete = Gen.oneOf (genScalar :| [ genName ])
+
+genInfix :: Int -> Gen ParsedExpr
+genInfix maxDepth
+  | maxDepth <= 0 = genConcrete
+  | otherwise = do
+    name <- Gen.elements ("*" :| infixNames)
+    p1 <- genExpr (maxDepth - 1)
+    p2 <- genExpr (maxDepth - 1)
+    pure $ Fn2 { name, p1, p2 }
+
+genExpr :: Int -> Gen ParsedExpr
+genExpr maxDepth
+  | maxDepth <= 0 = genConcrete
+  | otherwise = Gen.oneOf (genInfix maxDepth :| [])
--- test/TokenParser.purs
+++ test/TokenParser.purs
@@ -4,13 +4,14 @@ import Prelude
 import Control.Monad.Except (throwError)
 import Data.Either (Either(..))
 import Data.Tuple (Tuple(..))
+import Effect.Aff (Aff)
 import Expression (ParsedExpr)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import TestUtils (quickCheckParser)
-import Text.Parsing.Parser (ParseError, Parser, runParser)
+import Text.Parsing.Parser (Parser, runParser)
 import TokenParser (eof, tokenExprParser)
-import Tokenizer (Punctuation(..), TokenType(..), tokenStreamParser)
+import Tokenizer (TokenType(..), tokenStreamParser)
 
 spec :: Spec Unit
 spec = do
@@ -21,17 +22,18 @@ spec = do
       quickCheckParser (show :: Number -> String) chainP
     it "parses infix" do
       quickCheckParser (\(Tuple (a :: Int) (b :: Int)) -> (show a <> "*" <> show b)) chainP
+      quickCheckParser (\(Tuple (a :: Int) (b :: Int)) -> (show a <> "-" <> show b)) chainP
+    it "parses arbitrary expressions" do
+      quickCheckParser (show :: ParsedExpr -> String) chainP
     it "handles eof" do
       (runParser [] eof) `shouldEqual` (Right unit)
     it "handles manual numbers" do
       (show <$> runParser [ NumberTk "1" ] tokenExprParser) `shouldEqual` Right "1"
     it "handles function forms" do
-      runParser "reduce(1)" tokenStreamParser `shouldEqual` (Right [ NameTk "reduce", PunctuationTk OpenParen, NumberTk "1", PunctuationTk CloseParen ])
-      (show <$> runParser [ NameTk "reduce", PunctuationTk OpenParen, NumberTk "1", PunctuationTk CloseParen ] tokenExprParser) `shouldEqual` Right "reduce(1)"
-      (show <$> execP "reduce(1)") `shouldEqual` (Right "reduce(1)")
+      assertParse "reduce(1)"
     it "handles names" do
-      (show <$> execP "hello") `shouldEqual` Right "hello"
-      (show <$> execP "reduce") `shouldEqual` Right "reduce"
+      assertParse "hello"
+      assertParse "reduce"
     -- it "handles bignums properly" do
     -- (show <$> execP "0b101010101010101010101010101010101010101010101010101010101010101010") `shouldEqual` (Right "49191317529892137642")
     -- (show <$> execP "0B101010101010101010101010101010101010101010101010101010101010101010") `shouldEqual` (Right "49191317529892137642")
@@ -40,16 +42,10 @@ spec = do
     -- (show <$> execP "0o56123423423423523452345234512341256245365236462452345642") `shouldEqual` (Right "269870138443162641462903354048649622586931538414498")
     -- (show <$> execP "0O56123423423423523452345234512341256245365236462452345642") `shouldEqual` (Right "269870138443162641462903354048649622586931538414498")
     it "manual tests" do
-      (show <$> execP "1") `shouldEqual` (Right "1")
-      (show <$> execP "-1") `shouldEqual` (Right "-1")
-      (show <$> execP "463581") `shouldEqual` (Right "463581")
-      runParser "*" tokenStreamParser `shouldEqual` (Right [ InfixTk "*" ])
-      runParser "**" tokenStreamParser `shouldEqual` (Right [ InfixTk "*", InfixTk "*" ])
-      runParser "-1" tokenStreamParser `shouldEqual` (Right [ NumberTk "-1" ])
-      runParser "*-1" tokenStreamParser `shouldEqual` (Right [ InfixTk "*", NumberTk "-1" ])
-      runParser "-1*-1" tokenStreamParser `shouldEqual` (Right [ NumberTk "-1", InfixTk "*", NumberTk "-1" ])
-      (show <$> execP "1*1") `shouldEqual` (Right "1*1")
+      assertParse "2+-3"
+      assertParse "(-465650+-765144)+(-774230*-317464)"
 
+-- | Runs the tokenStreamParser and then tokenExprParser in sequence.
 chainP :: Parser String String
 chainP = do
   tokenStream <- tokenStreamParser
@@ -57,7 +53,5 @@ chainP = do
     Left err -> throwError err
     Right foo -> pure $ show foo
 
-execP :: String -> Either ParseError ParsedExpr
-execP input = do
-  tokenStream <- runParser input tokenStreamParser
-  runParser tokenStream tokenExprParser
+assertParse :: String -> Aff Unit
+assertParse input = runParser input chainP `shouldEqual` Right input
